901aaedddb45b146179e07715bc5b5708f2f9037
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-10-10 16:18:43 +0100

    Step 13

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -39,4 +39,28 @@ describe Bookmark do
       expect(Bookmark.all).not_to include bookmark
     end
   end
+
+  describe '.update' do
+    it 'updates the bookmark with the given data' do
+      bookmark = Bookmark.create(url: "www.google.com", title: "Google")
+      updated_bookmark = Bookmark.update(id: bookmark.id, url: 'www.notgoogle.com', title: 'Not Google')
+
+      expect(updated_bookmark).to be_a Bookmark 
+      expect(updated_bookmark.id).to eq bookmark.id
+      expect(updated_bookmark.title).to eq 'Not Google'
+      expect(updated_bookmark.url).to eq 'www.notgoogle.com'
+    end
+  end
+
+  describe '.find' do
+    it 'returns the requested bookmark object' do
+      bookmark = Bookmark.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+      result = Bookmark.find(id: bookmark.id)
+
+      expect(result).to be_a Bookmark
+      expect(result.id).to eq bookmark.id
+      expect(result.title).to eq 'Makers Academy'
+      expect(result.url).to eq 'http://www.makersacademy.com'
+    end
+  end
 end
